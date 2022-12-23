94be3bcd23141d53be82fa560a904f6c06aa0527
Sato Naoya <sato.naoya@classmethod.jp>
2022-12-23 11:31:24 +0900

    fix

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id INT PRIMARY_KEY, 
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
 	username TEXT NOT NULL,
 	email TEXT NOT NULL,
 	group_id INT,
@@ -11,7 +11,7 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
-	id INT PRIMARY_KEY, 
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
 	groupname TEXT NOT NULL,
 	created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
 	updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import {Hono} from 'hono';
 import {logger} from 'hono/logger';
 // import {} from 'hono/validator';
 import {jwt} from 'hono/jwt';
-import {D1QB} from 'workers-qb';
+import {D1QB, FetchTypes} from 'workers-qb';
 
 export interface Env {
   DB: D1Database;
   SECRET: string;
+}
+
+interface CreateUserBody {
+  username: string;
+  email: string;
+  groupId: string;
 }
 
 const app = new Hono<{Bindings: Env}>();
@@ -21,32 +27,37 @@ app.use('/auth/*', async (c, next) => {
 
 app.get('/', c => c.text('Hello World!!!'));
 
-interface CreateUserBody {
-  username: string;
-  email: string;
-  groupId: string;
-}
-
 app.post('/api/users', async c => {
-  try {
-    console.log('c', c);
+  console.log('c', c);
 
-    const body = await c.req.json<CreateUserBody>();
+  const body = await c.req.json<CreateUserBody>();
 
-    const qb = new D1QB(c.env.DB);
-    const inserted = await qb.insert({
-      tableName: 'users',
-      data: {
-        username: body.username,
-        email: body.email,
-        group_id: body.groupId,
-      },
-    });
-    return c.json(inserted);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+  const qb = new D1QB(c.env.DB);
+  const inserted = await qb.insert({
+    tableName: 'users',
+    data: {
+      username: body.username,
+      email: body.email,
+      group_id: body.groupId,
+    },
+  });
+  return c.json(inserted);
+});
+
+app.get('/api/users/:id', async c => {
+  console.log('c', c);
+
+  const {id} = c.req.param();
+  const qb = new D1QB(c.env.DB);
+  const fetched = await qb.execute({
+    query:
+      'SELECT users.id, users.username, users.email, groups.groupname FROM users JOIN groups ON users.id = groups.id WHERE users.id = ?1',
+    arguments: [id],
+    fetchType: FetchTypes.ONE,
+  });
+
+  console.log('fetched', fetched);
+  return c.json(fetched);
 });
 
 export default app;
